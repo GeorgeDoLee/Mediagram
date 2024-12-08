d40124851856843900ecfcfad6fcde32098722e5
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-12-08 18:51:12 +0400

    Added article controller

--- Mediagram/Controllers/ArticlesController.cs
+++ Mediagram/Controllers/ArticlesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mediagram.Common;
+using Mediagram.DTOs;
+using Mediagram.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mediagram.Controllers
@@ -7,5 +10,84 @@ namespace Mediagram.Controllers
     [ApiController]
     public class ArticlesController : ControllerBase
     {
+        private readonly ArticleService _articleService;
+        public ArticlesController(ArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllArticles()
+        {
+            var articles = await _articleService.GetAllArticlesAsync();
+
+            if (articles == null || !articles.Any())
+            {
+                return NotFound(new ApiResponse("No articles found."));
+            }
+
+            return Ok(new ApiResponse(true, "Articles retrieved successfully.", articles));
+
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            var article = await _articleService.GetArticleByIdAsync(id);
+
+            if (article == null)
+            {
+                return NotFound(new ApiResponse("Article not found."));
+            }
+
+            return Ok(new ApiResponse(true, "Article retrieved successfully.", article));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(ArticleDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest(new ApiResponse("Invalid article data."));
+            }
+
+            var article = await _articleService.AddArticleAsync(dto);
+
+            return Ok(new ApiResponse(true, "Article added successfully.", article));
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, ArticleDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest(new ApiResponse("Article data is invalid."));
+            }
+
+            var article = await _articleService.UpdateArticleAsync(id, dto);
+
+            if (article == null)
+            {
+                return NotFound(new ApiResponse("Article not found,"));
+            }
+
+            return Ok(new ApiResponse(true, "Article updated successfully.", article));
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var categoryDeleted = await _articleService.DeleteArticleAsync(id);
+
+            return categoryDeleted ?
+                Ok(new ApiResponse(true, "Article deleted successfully."))
+                :
+                NotFound(new ApiResponse("Article not found."));
+        }
     }
 }
