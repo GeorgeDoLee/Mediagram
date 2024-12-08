a835dc9ae22333eaf173447f3879b99794c025ae
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-12-08 18:41:19 +0400

    Added article dto

---
+++ Mediagram/DTOs/ArticleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mediagram.DTOs
+{
+    public class ArticleDto
+    {
+        public required string Title { get; set; }
+        public required int CategoryId { get; set; }
+        public required Dictionary<int, string> ArticleUrls { get; set; } = new Dictionary<int, string>(); 
+        // where kei is publisherId and value is url of article
+    }
+}
