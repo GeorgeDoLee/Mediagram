2e4e16b51ed19f839d714bb04b6de0567a627192
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-12-08 16:52:48 +0400

    Centralized Api Response

--- Mediagram/Common/ApiResponse.cs
+++ Mediagram/Common/ApiResponse.cs
@@ -2,5 +2,29 @@
 {
     public class ApiResponse
     {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object? Data { get; set; }
+        
+        public ApiResponse()
+        {
+            Success = true;
+            Message = "Request was successful.";
+            Data = null;
+        }
+
+        public ApiResponse(bool success, string message, object data = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+        
+        public ApiResponse(string message)
+        {
+            Success = false;
+            Message = message;
+            Data = null;
+        }
     }
 }
